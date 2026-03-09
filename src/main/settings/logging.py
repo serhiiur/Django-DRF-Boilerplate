29b5e7e1163ba485ed0448c3ed030391aedb58dd
prathamlahoti123 <prathamlahoti@duck.com>
2026-03-09 10:47:44 +0200

    Integrate JSON logger

--- src/main/settings/logging.py
+++ src/main/settings/logging.py
@@ -1,14 +1,24 @@
+import os
+
+from pythonjsonlogger.jsonlogger import JsonFormatter
+
+from main.constants import DjangoSettingsModule
 from main.env import env
 
+formatter = (
+  "verbose"
+  if os.environ["DJANGO_SETTINGS_MODULE"] == DjangoSettingsModule.DEVELOPMENT
+  else "json"
+)
 LOG_LEVEL = env.str("DJANGO_LOG_LEVEL", default="INFO").upper()
 LOGGING = {
   "version": 1,
   "disable_existing_loggers": False,
   "handlers": {
     "console": {
-      "class": "logging.StreamHandler",
       "level": LOG_LEVEL,
-      "formatter": "simple",
+      "class": "logging.StreamHandler",
+      "formatter": formatter,
     },
   },
   "loggers": {
@@ -18,9 +28,9 @@ LOGGING = {
     },
   },
   "formatters": {
-    "simple": {
-      "format": "{asctime} {levelname} {message}",
-      "style": "{",
+    "json": {
+      "()": JsonFormatter,
+      "format": "%(asctime)s %(levelname)s %(message)s %(module)s",
     },
     "verbose": {
       "format": "{asctime} {levelname} - {name}.{module}.py (line {lineno:d}). {message}",  # noqa: E501
